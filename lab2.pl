f2e700d23e781aac2b6c4fea810a862e91cf0be7
Bastian Soto <bastian.soto.j@usach.cl>
2021-06-07 14:51:07 -0400

    avance regla socialnetworkregister

--- lab2.pl
+++ lab2.pl
@@ -1,2 +1,26 @@
-fecha(Dia,Mes,Anio,[Dia,Mes,Anio]):- number(Dia),number(Mes),number(Anio), Dia > 0,
-    Mes > 0, Anio > 0, Dia =< 31, Mes =< 12.
+%fecha = [dia,mes,anio]
+fecha(Day,Month,Year,[Day,Month,Year]):- number(Day),number(Month),number(Year), Day > 0,
+    Month > 0, Year > 0, Day =< 31, Month =< 12.
+
+
+%socialnetwork=[nombre,fecha,sesionActiva,usuarios,publicaciones,comentarios]
+%comentarios es para utilizarse en funciones opcionales
+%se usa como referencia el tda visto en clases del profesor Roberto Gonzalez (vespertino)
+
+%usuario = [ID,username,password,date,listaSeguidores(contactos)]
+%usuarios= [IDultimoU,usuario1,usuario2,...]
+
+%publicacion = [ID,date,cantVecescompartidas,tipoContenido,contenido,listaUsernames,personasCompartidas,likes]
+%publicaciones= [IDultimoP,publicacion1,publicacion2,.....]
+
+%cantVecescompartidas y likes son para las funciones opcionales
+%comentario= [IDPost,ID,[ID,contenido,likes]]
+
+socialNetwork(N,Date,[N,Date,-1,[0],[0],[0]]):- string(N), fecha(_,_,_,Date).
+
+
+
+%encabezado
+%socialNetworkRegister(S1, “2021-05-01”, “user”, “pass”, S2)
+
+socialNetworkRegister([N,D,-1,[0],Ps,Cm],NewD,NU,NP,[N,D,-1,[1,[1,NU,NP,NewD,["seguidores"]]],Ps,Cm]).
