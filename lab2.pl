8f0c9ac810fbfecd32ac6796b41e1dcd5c7c8f01
Bastian Soto <bastian.soto.j@usach.cl>
2021-06-08 10:30:08 -0400

    avance predicado socialNetworkPost

--- lab2.pl
+++ lab2.pl
@@ -22,9 +22,9 @@ existeUsuario(Uid,U,P,D,UF,[[Uid,U,P,D,UF] |_]).
 existeUsuario(Uid,U,P,D,UF,[_|Us]):- existeUsuario(Uid,U,P,D,UF,Us).
 
 searchUser(Uid,U,P,D,UF,[[Uid,U,P,D,UF]|_], [Uid,U,P,D,UF]).
-searchUser(Uid,U,P,D,UF,[_|Us]):- searchUser(Uid,U,P,D,UF,Us).
+searchUser(Uid,U,P,D,UF,[_|Us],User):- searchUser(Uid,U,P,D,UF,Us,User).
 %--------------------------------------------------------------------------------------------------------------------------
-%listaFollowers(Usuarios,ListaVerificar)
+%listaFollowers(Usuario,ListaVerificar)
 listaFollowers([],_).
 listaFollowers([Head|Tail],ListaVerificar):- member(Head,ListaVerificar),
     listaFollowers(Tail,ListaVerificar).
@@ -47,7 +47,20 @@ socialNetworkLogin([N,D,-1,Us,Ps,Cm],U,P,[N,D,Uid,Us,Ps,Cm]):- existeUsuario(Uid
 %------------------------------------------------------------------------------------------------------------------------
 %ENCABEZADO
 %socialNetworkPost(Sn1, Fecha, Texto, ListaUsernamesDest,Sn2).
-%publicacion = [ID,date,cantVecescompartidas,tipoContenido,contenido,listaUsernames,personasCompartidas,likes]
+%publicacion = [ID,Username,date,cantVecescompartidas,tipoContenido,contenido,listaUsernames,personasCompartidas,likes]
 %caso ListaUsernamesDest = []
 %socialNetworkPost([N,D,Uid,Us,[0],Cm],F,TipoT,T,LU,)
-socialNetworkPost([N,D,Uid,[_|Us],[0],Cm],F,TipoT,T,[],[N,D,-1,Us,[1,[1,F,0,TipoT,T,[U],[0],[0]]],Cm]):- Uid > 0,
+socialNetworkPost([N,D,Uid,[_|Us],[0],Cm],F,TipoT,T,[],[N,D,-1,Us,[1,[1,U,F,0,TipoT,T,["dirigida a el mismo"],[0],[0]]],Cm]):-
+Uid > 0,
+string(TipoT),
+string(T),
+fecha(_,_,_,F),
+fecha(_,_,_,D),
+searchUser(Uid,U,_,_,_,_).
+
+socialNetworkPost([N,D,Uid,[Lid|Us],[LPid,[LPid,LP]|Ps],Cm],F,TipoT,T,LU,[N,D,-1,[Lid|Us],[NLPid,[NLPid,U,F,0,TipoT,T,LU,[0],[0]],[LPid,LP]|Ps],Cm]):-
+  Uid > 0,
+  NLPid is LPid + 1,
+  existeUsuario(Uid,U,_,_,_,Us),
+  searchUser(Uid,U,_,_,UF,Us,_),
+  listaFollowers(LU,UF).
